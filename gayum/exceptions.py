d87bc834bfb92b6cc7f907177595874880e8e096
Jensen Holm <jholm@cardinals.com>
2026-03-09 12:57:34 -0400

    project scaffold update

--- gayum/exceptions.py
+++
@@ -1,5 +0,0 @@
-class DataTypeError(Exception):
-    msg = 'data has an invalid datatype |{invalid_type}| must be narwhals supported dataframe'
-
-    def __init__(self, invalid_type: str):
-        super().__init__(self.msg.format(invalid_type=invalid_type))
