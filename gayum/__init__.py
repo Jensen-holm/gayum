84fea5acef76abf5cb4c664518eba386806d9cd2
Jensen Holm <jholm@cardinals.com>
2026-03-09 12:57:53 -0400

    formula class init implementation

--- gayum/__init__.py
+++ gayum/__init__.py
@@ -1,8 +1,9 @@
 from . import dists
 from . import terms
 from .gam import GAM
+from .formula import Formula
 
-__all__ = ['GAM', 'dists', 'terms']
+__all__ = ['GAM', 'Formula', 'dists', 'terms']
 
-def version() -> str:
-    return '0.1.0'
+
+__version__ = '0.1.0'
---
+++ gayum/formula.py
@@ -0,0 +1,16 @@
+import narwhals as nw
+from narwhals.typing import Frame
+
+from .terms import Term
+
+
+class Formula:
+    __slots__ = ['response', 'terms']
+
+    @nw.narwhalify
+    def __init__(self, df: Frame, response: str, terms: list[Term]):
+        assert response in df.columns
+        assert all([x.col in df.columns for x in terms])
+
+        self.terms = terms
+        self.response = response
